e773b1677e40d8b1c55958c8a967645ab826d5f4
GaragaNaveenKumar <23a35a0517@pragati.ac.in>
2025-05-18 14:08:38 +0530

    cart redesigned

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -15,7 +15,7 @@ const Cart = () => {
   const total = state.cartItems.reduce((sum, item) => sum + item.price * (item.quantity ||1), 0);
 
   return (
-    <div className="max-w-6xl mx-auto p-4 grid grid-cols-1 lg:grid-cols-3 gap-6">
+    <div className="max-w-6xl mt-10 mx-auto p-4 grid grid-cols-1 lg:grid-cols-3 gap-6">
       {/* Left: Cart Items */}
       <div className="lg:col-span-2">
         <h2 className="text-2xl font-bold mb-4">
